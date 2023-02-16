bbf8e6e5ace1b28e5d49fabc91c4abd71efc1281
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-02-16 16:10:50 +0100

    Create exo4.c

---
+++ exo4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#define SIZE 20
+
+
+void main () {
+
+    int tab[SIZE];
+    int valeur;
+    int somme=0;
+    int *ptab=&tab;
+    srand(time(NULL));
+
+    for(int i=0;i<SIZE;i++){
+
+        valeur=rand()%11;
+        *(ptab + i)=valeur;
+    }
+
+    for(int i=0;i<SIZE;i++){
+
+        somme +=*(ptab + i);
+    }
+
+    for(int i=0;i<SIZE;i++){
+
+        printf("%d\n",*(ptab + i));
+    }
+
+    printf("\nSomme : %d\n",somme);
+}
